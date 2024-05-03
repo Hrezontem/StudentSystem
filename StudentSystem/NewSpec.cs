11eee4333f234b2c05cdf66eb32a44ec117b1907
Hrezontem <nikitatarapata@gmail.com>
2024-05-03 19:20:40 +0300

    Revert "#По мелочи изменил: кнопки выхода теперь работают как возврат на MainForm"
    This reverts commit 519045d8ceb2700abbf95eace184c165914e8c2a.

--- StudentSystem/NewSpec.cs
+++ StudentSystem/NewSpec.cs
@@ -81,13 +81,5 @@ namespace StudentSystem
             this.Hide();
             MF.Show();
         }
-
-        private void NewSpec_FormClosing(object sender, FormClosingEventArgs e)
-        {
-            e.Cancel = true;
-            MainForm mainForm = new MainForm();
-            this.Hide();
-            mainForm.Show();
-        }
     }
 }
